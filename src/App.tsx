ba187556b83a0c606a4c0928698f78eb9800fbff
米高哥 <qc_69799f1c597a48e2b692280097438c7a@git.code.tencent.com>
2020-03-23 00:20:54 +0800

    init: project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "antd";
+import "./assets/scss/mi.scss";
+import "./styles/layout.scss";
+
+function App() {
+  return (
+    <div className="App flex align-center justify-center">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
